3a777f228cd7d42b9e6c337c6d97e5d2067dfbbf
bereketalem <bereketbruk@outlook.com>
2020-12-12 11:54:11 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <TaskToDo />
+  </div>
+</template>
+
+<script>
+import TaskToDo from './components/TaskToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskToDo
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Monsterrat:400,700');
+body{
+background-color: #EEEEEE;
+font-family: 'Monsterrat' , sans-serif;
+display:grid;
+grid-template-rows: auto;
+justify-items: center;
+align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+  width: 50%;
+}
+</style>
